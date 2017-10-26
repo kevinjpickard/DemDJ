84ee1941022e408a61c796002287d804ecdfd6d6
Kevin Pickard <kevin.pickard@jumpcloud.com>
2017-10-25 19:42:23 -0600

    Ginkgo working again and some example specs

--- DemDJ_suite_test.go
+++ DemDJ_suite_test.go
@@ -11,3 +11,19 @@ func TestDemDJ(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "DemDJ Suite")
 }
+
+// P<Verb> means the spec is pending
+var _ = PDescribe("server", func() {
+  var (
+    // server server
+    // error error
+  )
+
+  PDescribe("Starting server", func() {
+    PContext("When port 33635 is open", func() {
+      PIt("should start successfully, occupying port 33635", func() {
+
+      })
+    })
+  })
+})
